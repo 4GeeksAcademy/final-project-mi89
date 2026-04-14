22c77c427677f0b757ed463271b1dd5e04b12aae
Sean W. Hammond <138311759+Sean-Hammond@users.noreply.github.com>
2026-04-14 14:03:14 +0000

    Improve login/signup
    Styling, be able to log out from navbar

--- src/front/pages/Login.jsx
+++ src/front/pages/Login.jsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 import useGlobalReducer from "../hooks/useGlobalReducer";
 import { Navbar } from "../components/Navbar";
 import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
 
 export const Login = () => {
     const { store, dispatch } = useGlobalReducer();
@@ -90,12 +91,6 @@ export const Login = () => {
         return data
     }
 
-    // const handleSubmit = () => {
-    //     if (logInType === "Log In") {
-    //         navigate("/")
-    //     }
-    // }
-
     const getUser = async () => {
         const resp = await fetch(import.meta.env.VITE_BACKEND_URL + "/user", {
             method: "GET",
@@ -149,54 +144,66 @@ export const Login = () => {
         console.log("token after logging out:", store.userToken);
     }
 
-    // useEffect(() => {
-    //     getUser()
-    // }, [token])
+    useEffect(() => {
+        if (store.loggingOut == true) {
+            logout();
+            navigate("/");
+            dispatch({ type: "set_loggingOut", payload: false })
+            // This store variable will equal true only when the user clicks Log Out from the navbar, then it will immediately become false because of this useEffect, which will then log out and navigate to the home page.
+        }
+    }, [store.loggingOut])
 
     return (
-        <div>
+        <div id="login-page" className="bg-dark d-flex justify-content-center row mt-5">
             {store.userToken === null ?
-                (<form onSubmit={submitCredentials} className="mt-5 pt-5">
-                    <h2>{logInType === "Sign Up" && userType} {logInType}</h2>
-                    {logInType === "Sign Up" && <label><strong>I am a restaurant owner.</strong>
-                        <input
-                            type="checkbox"
-                            name="userIsOwner"
-                            checked={userType === "Owner"}
-                            onChange={(e) => {
-                                if (e.target.checked) {
-                                    setUserType("Owner")
-                                } else {
-                                    setUserType("Customer")
-                                }
-                            }}
-                        />
-                    </label>} <br />
-                    <small>{logInType === "Log In" ? "Don't" : "Already"} have an account? <span className="text-primary" onClick={() => { logInType === "Log In" ? setLogInType("Sign Up") : setLogInType("Log In") }}>Click here to {logInType === "Log In" ? "create one." : "log in."}</span></small> <br />
-                    <label>Username:
-                        <input
-                            type="username"
-                            value={username}
-                            onChange={(e) => { setUsername(e.target.value) }}
-                        />
-                    </label> <br />
-                    <label>Email:
-                        <input
-                            type="email"
-                            value={email}
-                            onChange={(e) => { setEmail(e.target.value) }}
-                        />
-                    </label> <br />
-                    <label>Password:
-                        <input
-                            type="password"
-                            value={password}
-                            onChange={(e) => { setPassword(e.target.value) }}
-                        />
-                    </label>
-                    <button className="btn btn-primary" type="submit" >{logInType}</button>
+                (<form onSubmit={submitCredentials} className="mt-5 p-4 card col-10 col-md-6 col-xl-4 my-5 rounded-4 border border-2 border-secondary">
+                    <h2 className="text-center">{logInType === "Sign Up" && userType} {logInType}</h2>
+                    <div>
+                        {logInType === "Sign Up" ? <label className="w-100 text-center"><strong>I am a restaurant owner. </strong>
+                            <input
+                                type="checkbox"
+                                name="userIsOwner"
+                                checked={userType === "Owner"}
+                                onChange={(e) => {
+                                    if (e.target.checked) {
+                                        setUserType("Owner")
+                                    } else {
+                                        setUserType("Customer")
+                                    }
+                                }}
+                            />
+                        </label> : <label className="w-100 text-center"><strong>Customers or owners can log in here.</strong></label>} <br />
+                        <label className="w-100">Username:
+                            <input
+                                className="w-100"
+                                type="username"
+                                value={username}
+                                onChange={(e) => { setUsername(e.target.value) }}
+                            />
+                        </label> <br />
+                        <label className="w-100">Email:
+                            <input
+                                className="w-100"
+                                type="email"
+                                value={email}
+                                onChange={(e) => { setEmail(e.target.value) }}
+                            />
+                        </label> <br />
+                        <label className="w-100">Password:
+                            <input
+                                className="w-100"
+                                type="password"
+                                value={password}
+                                onChange={(e) => { setPassword(e.target.value) }}
+                            />
+                        </label>
+                    </div>
+                    <button className="btn my-3 fw-bold" type="submit" >{logInType}</button>
+                    <span className="mb-3 text-center"><button className="w-100 login-type-button" onClick={() => { logInType === "Log In" ? setLogInType("Sign Up") : setLogInType("Log In") }}>I {logInType === "Log In" ? "don't" : "already"} have an account</button></span>
                 </form>)
-                : (<button type="button" onClick={logout} className="pt-5 mt-5">Log Out</button>)}
+                : (<div><p className="loading">Loading...</p>
+                <p className="loading-message">If this is going too slow, <Link to="/">click here to go home.</Link></p>
+                </div>)}
         </div>
     )
 }
